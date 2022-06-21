2ba3787e7933f7bb90904aed77fb9dc579e1af64
Stokoloko@89 <lungaqaba@gmail.com>
2022-06-21 10:13:24 +0200

    2.11 done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Persons from "./components/Persons";
 import PersonForm from "./components/PersonsForm";
 import SearchFilter from "./components/SearchFilter";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
-  const [query, setQuery] = useState([])
+  const [query, setQuery] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then((response) => setPersons(response.data));
+  }, []);
 
   const addNumber = (event) => {
     event.preventDefault();
@@ -46,10 +48,7 @@ const App = () => {
   const getFilteredPersons = (query, persons) =>
     !query
       ? persons
-      : persons.filter((person) =>
-        person.name.toLowerCase().includes(query))
-    ;
-
+      : persons.filter((person) => person.name.toLowerCase().includes(query));
   const filteredPersons = getFilteredPersons(query, persons);
 
   return (
@@ -66,9 +65,7 @@ const App = () => {
         onNumChange={newPersonNumber}
       />
       <h2>Numbers</h2>
-      <Persons persons={filteredPersons}>
-        {" "}
-      </Persons>
+      <Persons persons={filteredPersons}> </Persons>
     </div>
   );
 };
